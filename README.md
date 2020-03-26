486541ed2924e7a207f9a33ab3ad0ca2d3edd36e
ariana <arcursino@gmail.com>
2020-03-26 19:53:05 -0300

    inclui mobile

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Curso da Semana Omnistack 11
 
+#Be the Hero!!!
+
 Protótipo de um Sistema Web e seu app utilizando node.js
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello Omnistack</code> and save to reload.
+          Hello World!!!!
         </p>
         <a
           className="App-link"
